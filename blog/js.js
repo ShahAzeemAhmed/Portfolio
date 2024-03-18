3f2a1fdab8720b29a21a979f1165f9ccf6b26d29
ShahAzeem Ahmed <95062342+ShahAzeemAhmed@users.noreply.github.com>
2024-03-19 01:00:38 +0530

    A
    A

---
+++ blog/js.js
@@ -0,0 +1,27 @@
+// FILTER JS
+$(document).ready(function() {
+    $('.filter-item').click(function() {
+        const value = $(this).attr('data-filter')
+        if (value == 'all') {
+            $('.post-box').show('1000')
+        } else {
+            $('.post-box')
+                .not('.' + value)
+                .hide("1000");
+            $('.post-box')
+                .filter('.' + value)
+                .show("1000");
+        }
+    });
+    // add acitve btn
+    $('.filter-item').click(function() {
+        $(this).addclass("active-filter").siblings().removeClass("active-filter");
+    });
+});
+//header bg chg on sll
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+
+});
